de10b9b5176f21e2d2d09005714b88d2af538afb
qaher <2097349@talnet.nl>
2020-09-09 08:53:59 +0200

    De code is versimpeld

--- signup.php
+++ signup.php
@@ -1,41 +1,17 @@
 <?php
 
-if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['repeat_password'])) {
+if( isset($_POST['password']) && isset($_POST['repeat_password'])) {
 
-  if (empty($_POST['firstname'])){
-    echo "Firstname is required!";
-  }
 
-  elseif (empty($_POST['lastname'])) {
-    echo "Lastname is required!";
-  }
 
-  elseif (empty($_POST['email'])) {
-    echo "Email is required!";
-  }
-
-  elseif (empty($_POST['username'])) {
-    echo "Username is required!";
-  }
-
-  elseif (empty($_POST['password'])) {
-    echo "Password is required!";
-  }
-
-  elseif(empty($_POST['repeat_password'])){
-    echo "Repeat password is required!";
-  }
-
-  elseif ($_POST['password'] != $_POST['repeat_password']) {
+  if ($_POST['password'] != $_POST['repeat_password']) {
     echo "Passwords do not match!";
   }
 
-  else{
-    echo "You can now log in have fun.";
-  }
 
 
 }
+
 else{
   echo "";
 }
@@ -56,27 +32,29 @@ else{
 <form class="" action="signup.php" method="post">
 
   <label for="firstname">First name:</label><br>
-  <input type="text" name="firstname" value=""><br>
+  <input type="text" name="firstname" value="" required><br>
 
   <label for="tussenvoegsels">insertion:</label><br>
   <input type="text" name="tussenvoegsels" value=""><br>
 
   <label for="lastname">Last name:</label><br>
-  <input type="text" name="lastname" value=""><br>
+  <input type="text" name="lastname" value="" required><br>
 
   <label for="email">email:</label><br>
-  <input type="email" name="email" value=""><br>
+  <input type="email" name="email" value="" required><br>
 
   <label for="username">Username:</label><br>
-  <input type="text" name="username" value=""><br>
+  <input type="text" name="username" value="" required><br>
 
   <label for="password">Password:</label><br>
-  <input type="password" name="password" value=""><br>
+  <input type="password" name="password" value="" required><br>
 
   <label for="repeat_password">Repeat Password:</label><br>
-  <input type="password" name="repeat_password" value=""><br><br>
+  <input type="password" name="repeat_password" value="" required><br><br>
 
   <button type="submit" name="Register">Register</button>
+
+  <a href="index.php">Ik heb al een account signup</a>
 
 </form>
 
