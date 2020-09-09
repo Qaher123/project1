e29c38f0732b0ed0c259987f1878e8aa5e73ebbc
qaher <2097349@talnet.nl>
2020-09-09 10:16:38 +0200

    dit is de sql file die ik heb gemaakt voor dit prject

---
+++ sql_statements.sql
@@ -0,0 +1,20 @@
+CREATE TABLE account(
+    account_id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    PASSWORD VARCHAR(255) NOT NULL,
+    PRIMARY KEY(account_id)
+);
+
+CREATE TABLE persoon(
+    
+    persoon_id INT NOT NULL AUTO_INCREMENT,
+    first_name varchar(255),
+    insertion varchar(255),
+    lastname varchar(255),
+    username varchar(255),
+    account_id INT NOT NULL,
+    
+    PRIMARY KEY (persoon_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+    
+);
